4b39aceb1d1da608b24d6d190549ec77cf5acb09
shenwl <shenweilun96@gmail.com>
2020-07-26 21:17:20 +0800

    寻路问题基本设置：可保存的画布

--- demos/path/index.ts
+++ demos/path/index.ts
@@ -1,21 +1,57 @@
-const map: number[] = new Array(10000).fill(0);
-const root: HTMLDivElement = document.getElementById('root') as HTMLDivElement;
-let mouse: boolean = false
+const cacheMap = localStorage.getItem('map');
+let map: number[] = cacheMap ? JSON.parse(cacheMap) : new Array(10000).fill(0);
+const container: HTMLDivElement = document.getElementById('container') as HTMLDivElement;
+const saveBtn: HTMLButtonElement = document.getElementById('save-btn') as HTMLButtonElement;
+const clearBtn: HTMLButtonElement = document.getElementById('clear-btn') as HTMLButtonElement;
+let mouse: boolean = false;
+let clear: boolean = false;
 
-function __main__() {
-  for(let y = 0; y < 100; y++) {
+function onSave() {
+  localStorage.setItem('map', JSON.stringify(map));
+}
+function onClear() {
+  localStorage.removeItem('map');
+  map = new Array(10000).fill(0);
+  container.innerHTML = '';
+  drawMap();
+}
+
+function drawMap() {
+  for (let y = 0; y < 100; y++) {
     for (let x = 0; x < 100; x++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
+      const position = y * 100 + x;
+      if (map[position] === 1) {
+        cell.style.backgroundColor = 'black';
+      }
       cell.addEventListener('mousemove', () => {
-        if(mouse) {
-          cell.style.backgroundColor = 'black';
+        if (mouse) {
+          cell.style.backgroundColor = clear ? '' : 'black';
+          map[position] = clear ? 0 : 1;
         }
       })
-      root.appendChild(cell);
+      container.appendChild(cell);
     }
   }
-  window.document.addEventListener('mousedown', () => mouse = true);
+}
+
+function initEvent() {
+  window.document.addEventListener('mousedown', (e: MouseEvent) => {
+    mouse = true
+    // 右键
+    clear = e.which === 3;
+  });
   window.document.addEventListener('mouseup', () => mouse = false);
+
+  window.document.addEventListener('contextmenu', (e: Event) => e.preventDefault());
+
+  saveBtn.addEventListener('click', onSave);
+  clearBtn.addEventListener('click', onClear);
+}
+
+function __main__() {
+  drawMap();
+  initEvent();
 }
 __main__();
