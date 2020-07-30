4460e89e4b3c7eeadef49af09851804774e103d1
shenwl <shenweilun96@gmail.com>
2020-07-30 22:33:51 +0800

    使用MinBinaryHeap，优化性能

--- demos/path/sorted.ts
+++ demos/path/sorted.ts
@@ -1,43 +1,32 @@
+import MinBinaryHeap from '../../common/minBinaryHeap';
+
 /**
  * 乱序存入元素，有序取出
  * @todo: 数据结构用二叉树优化下
  */
 export default class Sorted<T> {
-  data: T[];
+  heap: MinBinaryHeap<T>;
   compare: Function;
 
   constructor(data: T[], compare: (a: T, b: T) => number) {
-    this.data = data;
+    this.heap = new MinBinaryHeap(data, compare);
     this.compare = compare;
   }
 
   get length(): number {
-    return this.data.length;
+    return this.heap.size;
   }
 
   // 取最小的
   take = (): T | null => {
-    const { data } = this;
-    if (!data || (data.length < 1)) return null;
-    let min = data[0];
-    let minIndex = 0;
-    for (let i = 1; i < data.length; i++) {
-      if (this.compare(data[i], min) < 0) {
-        min = data[i];
-        minIndex = i;
-      }
-    }
-    // 删除元素
-    data[minIndex] = data[data.length - 1];
-    this.data.pop();
-    return min;
+    return this.heap.takeMin();
   }
 
   insert(el: T) {
-    this.data.push(el);
+    this.heap.insert(el);
   }
 
-  isEmpty = () => {
-    return this.data.length === 0;
+  isEmpty = (): boolean => {
+    return this.heap.isEmpty();
   }
 }
