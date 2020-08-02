42c1f0558151b5cf748b3fe9e201ab78832a8e0b
shenwl <shenweilun96@gmail.com>
2020-08-02 13:10:03 +0800

    在文字中拖动

--- demos/range/dragable.ts
+++ demos/range/dragable.ts
@@ -1,11 +1,13 @@
 type DragableOptions = {
   el: string,
+  onMove?: (e: MouseEvent, dragable: HTMLBaseElement) => void;
 }
 
 export default class Dragable {
   $el: HTMLBaseElement;
   baseX: number;
   baseY: number;
+  onMove?: (e: MouseEvent, dragable: HTMLBaseElement) => void;
 
   constructor(options: DragableOptions) {
     const $el: HTMLBaseElement = document.querySelector(options.el);
@@ -15,17 +17,19 @@ export default class Dragable {
     this.$el = $el;
     this.baseX = 0;
     this.baseY = 0;
+    this.onMove = options.onMove;
 
     this.init();
   }
 
   init() {
+    const { onMove } = this;
     const dragable = this.$el;
     dragable.addEventListener('mousedown', (e: MouseEvent) => {
       let startX: number = e.clientX;
       let startY: number = e.clientY;
 
-      const move = (e: MouseEvent) => {
+      const move = onMove ? (e: MouseEvent) => onMove(e, dragable) : (e: MouseEvent) => {
         const { clientX, clientY } = e;
         const x = clientX - startX + this.baseX;
         const y = clientY - startY + this.baseY;
--- demos/range/index.ts
+++ demos/range/index.ts
@@ -1,3 +1,35 @@
 import Dragable from './dragable';
 
-new Dragable({ el: '#dragable' })
+const root = document.getElementById('root') as HTMLDivElement;
+
+let ranges: Range[] = [];
+document.addEventListener('selectstart', e => e.preventDefault());
+
+for (let i = 0; i < root.childNodes[0].textContent.length; i++) {
+  let range = document.createRange();
+  range.setStart(root.childNodes[0], i);
+  range.setEnd(root.childNodes[0], i);
+  ranges.push(range);
+}
+
+function nearest(x0: number, y0: number): Range {
+  let nearestRange = null;
+  let distance = Infinity;
+
+  for (let range of ranges) {
+    const { x, y } = range.getBoundingClientRect();
+    const d = (x0 - x) ** 2 + (y0 - y) ** 2;
+    if(d < distance) {
+      nearestRange = range;
+      distance = d;
+    }
+  }
+  return nearestRange;
+}
+
+new Dragable({ el: '#dragable', onMove: (e: MouseEvent, dragable) => {
+  const { clientX, clientY } = e;
+  const range = nearest(clientX, clientY);
+  console.log(range);
+  range.insertNode(dragable)
+} });
